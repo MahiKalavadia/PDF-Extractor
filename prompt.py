0fc97062a66206739a21e5d2869c1b6dbe2d2971
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-06-24 18:18:19 +0530

    Changed the prompt for Amount details

--- prompt.py
+++ prompt.py
@@ -11,13 +11,28 @@ You are an expert/professional visual data analyser who extracts data from invoi
 **Role: This is the seller who is generating this invoice (found in "Seller", "From", "Service Provider", "Billed By", "Consigner", or "Ship From" section)
 **Dealer GSTIN(dealer_gstin): Rule: If the value doesn't contain HIIB's corporate PAN `AAGCH0310P` consider it as dealer_gstin value.(Eg. `08AAFC8783J12` )
 **Related fields: dealer_gstin, dealer_code, dealer_pincode, dealer_state_code, dealer_pan, consigner_details, consigner_address, consigner_pincode, consigner_place_of_supply
+3. Amount Details:
+**Role: What the buyer has purchased its price and GST amount.
+**Related fields: taxable_value, sgst_amount, cgst_amount, igst_amount, total_invoice
+**Steps:
+  - Search for billing table/section.
+  - Identify row/column containing field names: taxable_value, sgst_amount, cgst_amount, igst_amount, total_invoice
+  - Map each field names to its value
+  - Extract each field and its value
+**Instruction: Never print any field value to another field value. Like -> SGST value inside IGST value.
+**If found CGST value then SGST value must be present. If found only one then it is IGST value.
+
 
 **GENERAL INSTRUCTIONS**
 1. Extract only those data which are directly visible inside the image. Do not hallucinate any extra data for extraction.
 2. If any field is not explicility present inside the image then return empty string.
 3. Date format should be `DD-MM-YYYY`. If month name is explicility present like Jan/January then convert them to its numeric value (eg. Jan -> 01)(eg. May -> 05)(eg. Nov -> 11).
 4. No OCR/Alignment errors: Be extremely careful to differentiate similar characters (e.g., number '0' vs letter 'O', number '1' vs letter 'I', number '2' vs letter 'Z').
-5. Account number must contain digits only. No extra spaces, hypens attached.
+5. Account number must contain digits only. No extra spaces, hypens attached. No extra digit added or removed. Look especially where multiple zeros are attached together.
+7. Look for compressed values too.
+8. Never add extra 0 where multiple zeros are present. Look carefully before interpreting it.
+9. If dealercode and oem not found zoom and look inside Buyer's Order No. 
+10. If MSME code not found, look for any value starting with UDYAM and if found extract it
 
 ### EXTRACTION GUIDES
 1. Invoice and Reference Details:
@@ -27,21 +42,25 @@ You are an expert/professional visual data analyser who extracts data from invoi
 - `invoice_number`: The invoice number same as mentioned.
 - `invoice_date`: The invoice date in format `DD-MM-YYYY`.
 
-2. Amount Details:
-- `taxable_value`: The total taxable amount before GST got applied.
-- `cgst_amount`: The Central GST(CGST) Amount.
-- `sgst_amount`: The State GST(SGST) Amount.
-- `igst_amount`: The Integrated GST(IGST) Amount.
-- 'total_invoice`: The total amount of the purchased good after GST amount got applied.
+2. Billing Amount:
+- `taxable_value`: Taxable value means the amount before GST amount got added.
+- `cgst_amount`:  
+- `sgst_amount`: 
+- `igst_amount`: 
+- `total_invoice`: Total invoice means the amount after GST amount got added.
 
 3. Codes:
-- `dealer_code`: The dealer code with a length of 5. Must start with a letter(eg. `M3251` , `H7435`). Search inside headers, service providers information, remarks.
+- `dealer_code`: The dealer code with a length of 5. Must start with a letter.Search inside headers, service providers information, remarks, Buyer's Order No.
 - `hiib_misp_code`: The HIIB MISP Code. Format => HIIB-MHY-XXXX where X is a digit. If value found as MHY-XXXX then prepend `HIIB-`.
 
 4. Bank information/Details:
+eg: HDFC Bank & HDFC0847351
+    Bank name: HDFC 
+    IFSC Code: HDFC0847351
+Step1: Calculate length of account_number firstly and then extract and check if both of their lengths are same. If not print null
 - `account_holders_name`: Account holder's name. Only if explicility mentioned along with other bank details.
 - `bank_name`: Bank Name
-- `account_number`: Bank Account number
+- `account_number`: Inside bank details
   ** Branch and IFSC Code must be mentioned together.(eg. Branch & IFS Code: HDFC Bank & HDFC0847351)
 - `branch`: Bank Branch Name(only branch name. Do not include IFSC Code here.)
 - `bank_ifsc_code`: The 11 character IFSC code only.
@@ -53,8 +72,9 @@ You are an expert/professional visual data analyser who extracts data from invoi
 - `dealer_pan`: The 10 character long dealer PAN.(Character from position 3 to 12 of dealers_gstin).
 
 6. Address details:
-- `hiib_pincode`: The 6 digit numeric pincode value. Found in HIIB address/Buyers address. Step1: Search for Buyer(Bill TO) section. Step2: Identify the hiib address/buyers address. Step3: Extract 6 digit long pincode mentioned at last.
-- `dealer_pincode`: The 6 digit long pincode inside seller's/service provider's address.(Do not search inside Buyer(Bill To)section.)
+   For HIIB: Search for Buyer(Bill To)/Consignee and extract 6 digit numeric value from its address
+- `hiib_pincode`: The 6 digit numeric pincode value. Found in HIIB address/Buyers address.
+- `dealer_pincode`: The 6 digit long pincode inside seller's/service provider's/consigners address.(Do not search inside Buyer(Bill To)section.) 
 - `hiib_state_code`: The 2 digit numeric state code value inside HIIB's address.
 - `dealer_state_code`: The 2 digit numeric state code value inside dealer's address.
 
@@ -71,9 +91,9 @@ You are an expert/professional visual data analyser who extracts data from invoi
 8. Other Details:
 - `sac`: The Service Accounting Code(Usually 6 digit long).
 - `msme`: The MSME registration number. Usually starts with UDYAM-.(Search in headers, footers, remarks, description of service section).
-- `oem`: The Original Equipment Manufacturer(oem) name. (eg. "HYUNDIA","MARUTI","HONDA" etc). Return only name value.
-- `quantity`: The quantity value inside billing table. If quantity is not explicitly shown, take it from the description of service when a clear value count is present there. If no quantity can be determined, return 0.
+- `oem`: The Original Equipment Manufacturer(oem) name. (eg. "HYUNDIA","MARUTI","HONDA" etc). Return only name value. Search inside headers, footers, remarks, billing table, Buyer's Order No.. Can be represented as OEM Name: , Or if not found search who is consignee/Buyer(bill to) and extract its first name like hyundia/maruti/suzuki and print it.
 - `description_of_service`: The description of service inside the billing section table.
+- `quantity`: The quantity value inside billing table. If quantity column/row is empty then search inside description of service(how many description of service has extracted and count its value)
 - `period_of_service`: The service period. If found across multiple lines concatenate it. (Search inside headers, footer, remarks, billing table)
 ### OUTPUT JSON FORMAT:
 Return a JSON object matching this schema exactly:
